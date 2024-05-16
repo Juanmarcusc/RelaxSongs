271094517a4690e9317289e8c43d12d9168b3f43
Juan Marcus <eng.juanmarcus@gmai.com>
2024-05-16 10:40:34 -0300

    firt commit

---
+++ my-react-app/src/Api.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+const PORT = 3000; 
+const API_KEY = 'AIzaSyC0Irgp6XwyZWJN1HTN1w7BLpxLk4eSg3w'
+
+app.get('/api/song', async (req, res) => {
+    try {
+      const { q } = req.query;
+      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          part: 'snippet',
+          q: q,
+          key: API_KEY,
+        },
+      });
+  
+      const videoId = response.data.items[0].id.videoId;
+      const snippet = response.data.items[0].snippet;
+      res.json({
+        title: snippet.title,
+        artist: snippet.channelTitle,
+        thumbnailUrl: snippet.thumbnails.default.url,
+        videoUrl: `https://www.youtube.com/watch?v=${videoId}`,
+      });
+    } catch (error) {
+      console.error('Erro ao buscar música:', error);
+      res.status(500).json({ error: 'Falha ao buscar música' });
+    }
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Servidor está rodando na porta ${PORT}`);
+  });
---
+++ my-react-app/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [song, setSong] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('')
+  
+  const fetchSong = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`http://localhost:3000/api/song?q=${searchTerm}`);
+      setSong(response.data);
+    } catch (error) {
+      console.error('Error fetching song:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+  return (
+     <div className="App">
+      <h1>Relax Songs</h1>
+      <input placeholder="Procurar Música" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}></input>
+      <button onClick={fetchSong} disabled={loading}>Buscar</button>
+      {song && (
+        <div>
+          <h2>{song.title}</h2>
+          <p>Artist: {song.artist}</p>
+          <audio controls>
+            <source src={song.audioUrl} type="audio/mpeg" />
+          </audio>
+        </div>
+      )}
+    </div>
+
+  );
+}
+
+export default App;
